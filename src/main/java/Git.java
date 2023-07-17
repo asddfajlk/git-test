8a767aa26cf9ae59000a7e5d17f84f5bb9e0a193
jiangHuOUtCast <3086159804@qq.com>
2023-07-17 23:09:53 +0800

    git commit

--- src/main/java/Git.java
+++ src/main/java/Git.java
@@ -1,5 +1,8 @@
 public class Git {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello git");
+        System.out.println("hello 太难了");
+        System.out.println("hello 我不知所措");
     }
 }
